69339bd019e2a20613d175adbab1108329b929b1
elisethedev <110583073+elisethedev@users.noreply.github.com>
2023-01-12 21:52:09 -0700

    first commit

---
+++ src/app/nav/nav.component.spec.ts
@@ -0,0 +1,45 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+
+import { NavComponent } from './nav.component';
+import { AuthService } from '../services/auth.service';
+
+describe('NavComponent', () => {
+  let component: NavComponent;
+  let fixture: ComponentFixture<NavComponent>;
+  const mockedAuthService = jasmine.createSpyObj(
+    'AuthService',
+    ['createUser', 'logout'],
+    {
+      isAuthenticated$: of(true),
+    }
+  );
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NavComponent],
+      imports: [RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: mockedAuthService }],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+
+  it('should logout', () => {
+    const logoutLink = fixture.debugElement.query(By.css('li:nth-child(3) a'));
+
+
+    logoutLink.triggerEventHandler('click');
+
+    const service = TestBed.inject(AuthService);
+
+  
+})})
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCScj8kNiMjhNjNmO2Wq-EB1n6Bh_VYgkE",
+    authDomain: "clips-84b28.firebaseapp.com",
+    projectId: "clips-84b28",
+    storageBucket: "clips-84b28.appspot.com",
+    messagingSenderId: "268459747684",
+    appId: "1:268459747684:web:63c6af4b124c0fb72b7271"
+  }
+};
